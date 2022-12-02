a32d0431d0825d8d1337da133380a2b9bc98efb3
Ben Mcleod <ben-mcleod@live.co.uk>
2022-12-02 23:00:51 +0000

    Basic feed layout

--- App.js
+++ App.js
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.appContainer}>
+            <View>
+                <TextInput placeholder="What's on your mind?" />
+                <Button title="Post" />
+            </View>
+            <View style={styles.postsContainer}>
+                <View style={styles.postContainer}>
+                    <View style={styles.postVotesContainer}>
+                        <Text>Votes</Text>
+                    </View>
+                    <View style={styles.postContentAndDetailsContainer}>  
+                        <View  style={styles.postContentContainer}>
+                            <Text>Post no. 1</Text>
+                        </View>
+                        <View style={styles.postDetailsContainer}>
+                            <Text>1m ago --- 2 km away</Text>
+                        </View>
+                    </View> 
+                </View>
+
+                <View style={styles.postContainer}>
+                    <View style={styles.postVotesContainer}>
+                        <Text>Votes</Text>
+                    </View>
+                    <View style={styles.postContentAndDetailsContainer}>  
+                        <View  style={styles.postContentContainer}>
+                            <Text>Post no. 2</Text>
+                        </View>
+                        <View style={styles.postDetailsContainer}>
+                            <Text>5m ago --- 3 km away</Text>
+                        </View>
+                    </View> 
+                </View>
+
+                <View style={styles.postContainer}>
+                    <View style={styles.postVotesContainer}>
+                        <Text>Votes</Text>
+                    </View>
+                    <View style={styles.postContentAndDetailsContainer}>  
+                        <View  style={styles.postContentContainer}>
+                            <Text>Post no. 3</Text>
+                        </View>
+                        <View style={styles.postDetailsContainer}>
+                            <Text>15m ago --- 1 km away</Text>
+                        </View>
+                    </View> 
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    appContainer: {
+        flex: 1,
+        backgroundColor: 'cyan',
+        alignItems: 'center',
+        padding: 20,
+    },
+    postsContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        borderWidth: 1,
+        width: '100%',
+    },
+    postContainer: {
+        flexDirection: 'row',
+        margin: 2,
+        borderWidth: 1,
+        padding: 2,
+        height: 100,
+    },
+    postContentAndDetailsContainer: {
+        flex: 1,
+    },
+    postVotesContainer: {
+        margin: 1,
+        borderWidth: 1,
+        padding: 1,
+    },
+    postContentContainer: {
+        flex: 1,
+        margin: 1,
+        borderWidth: 1,
+        padding: 1,
+    },
+    postDetailsContainer: {
+        margin: 1,
+        borderWidth: 1,
+        padding: 1,
+        height: '30%',
+    },
 });
